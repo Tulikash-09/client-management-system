e4b56273cc0e06efc17876ceb13a2f129a65ac3a
Tulikash-09 <stulika029@gmail.com>
2025-01-05 01:26:11 -0500

    Search bar for meetings. Implemented ViewChild & AfterViewInit

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-view',
@@ -8,12 +8,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './view.component.html',
   styleUrl: './view.component.css'
 })
-export class ViewComponent implements OnInit{
+export class ViewComponent implements OnInit, AfterViewInit{
+
+  @ViewChild('filterInput', {static: true}) filterInput: ElementRef | undefined;
+  @ViewChildren('meetingRow') meetingRow: QueryList<ElementRef> | undefined;
 
   meetingInfo:any[] = [];
+  filteredMeetings:any[] = [];
   selectedMeeting:any = null;
+  loading: boolean = true;
 
   constructor(private http:HttpClient){}
+
+  ngAfterViewInit(): void {
+    if(this.filterInput){
+      this.filterInput.nativeElement.focus();
+    }
+
+    this.meetingRow?.forEach(row => {
+      row.nativeElement.addEventListener('mouseover', () =>{
+        row.nativeElement.style.backgroundColor = 'lightgray';
+      });
+      row.nativeElement.addEventListener('mouseout', () =>{
+        row.nativeElement.style.backgroundColor = '';
+      });
+    });
+  }
 
   ngOnInit(): void {
       this.fetchDetails();
@@ -21,14 +41,18 @@ export class ViewComponent implements OnInit{
 
   fetchDetails(){
     const clientId = localStorage.getItem('clientId');
+    this.loading = true;
     // console.log('view.component.ts:',clientId);
     this.http.get(`http://localhost:5500/view/${clientId}`).subscribe(
       (response:any)=>{
         // console.log(response[0]);
         this.meetingInfo = response;
+        this.filteredMeetings = [...this.meetingInfo];
+        this.loading = false;
       },
     (error:any)=>{
       console.error('Error in loading the details',error);
+      this.loading = false;
     }
     );
   }
@@ -39,5 +63,18 @@ export class ViewComponent implements OnInit{
 
   clearSelection(){
     this.selectedMeeting = null;
+  }
+
+  filterMeetings(topic:string){
+    this.filteredMeetings = this.meetingInfo.filter(
+      (meeting:any)=>
+        meeting.topic.toLowerCase().includes(topic.toLowerCase())
+    );
+    if(this.filterMeetings.length > 0){
+      this.selectedMeeting = this.filteredMeetings[0];
+    }else{
+      this.selectedMeeting = null;
+    }
+
   }
 }
