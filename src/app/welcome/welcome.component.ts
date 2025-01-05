12002a26aa1122a3370e223b349d96ab8c903dc1
Tulikash-09 <stulika029@gmail.com>
2025-01-04 19:01:41 -0500

    View Meeting Details added on Welcome Page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-welcome',
@@ -11,16 +12,19 @@ import { AuthService } from '../auth.service';
 export class WelcomeComponent implements OnInit{
 
   userName:string='';
+  clientId:number = 0;
 
-  constructor(private authService:AuthService){}
+  constructor(private authService:AuthService, private http:HttpClient){}
 
   ngOnInit(): void {
       const name = this.authService.getUserName();
       // console.log('Fetched userName:',name);
       if(name){
         this.userName = name;
+        this.clientId = Number(localStorage.getItem('clientId'));
       }else{
         console.error('No Username found');
       }
   }
+
 }
